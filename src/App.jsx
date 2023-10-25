5924e6e413b1001b2cad34425c5ac72701ee618d
maximo cavero <cavero.maximo.l@gmail.com>
2023-10-25 01:21:58 -0300

    agregando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { Navbar } from './assets/components/Navbar'
+import {Home} from  './assets/pages/Home'
+import {Contacto} from './assets/pages/Contacto'
+import {Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <>
+     <Navbar></Navbar>
+     <Routes>
+      <Route path='/Home' element={<Home/>}/>
+      <Route path='/contacto' element={<Contacto/>}/>
+     </Routes>
+     <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className="h-screen bg-white dark:bg-gray-900 ">
+    <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        Home
+    </div>
+    </div>
+    
+  )
+}
